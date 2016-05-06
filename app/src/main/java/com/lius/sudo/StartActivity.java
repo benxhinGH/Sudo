4f8d9d82c61b39a5775a1f505af3f3dbf5eac521
lius <824797605@qq.com>
2016-05-06 11:09:21 +0800

    增加自定义loadingdialog

--- app/src/main/java/com/lius/sudo/StartActivity.java
+++ app/src/main/java/com/lius/sudo/StartActivity.java
@@ -1,42 +1,77 @@
 package com.lius.sudo;
 
 import android.app.Activity;
-import android.app.ProgressDialog;
+import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 
 
 /**
  * Created by Administrator on 2016/4/12 0012.
  */
-public class StartActivity extends Activity{
+public class StartActivity extends Activity {
 
-    public static int level=1;
+    final int SHOW_LOADING_DIALOG=0;
+    final int CLOSE_LOADING_DIALOG=1;
+
+    public static int level = 1;
     private Button startGameButton;
     private Button levelButton;
-    private ProgressDialog progressDialog;
+    private Dialog mLoadingDialog;
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what){
+                case SHOW_LOADING_DIALOG:
+                    showProgressDialog();
+                    break;
+                case CLOSE_LOADING_DIALOG:
+                    closeProgressDialog();
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.startlayout);
-        startGameButton=(Button)findViewById(R.id.startgame_button);
+        startGameButton = (Button) findViewById(R.id.startgame_button);
         startGameButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showProgressDialog();
+
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
-                        GenerateSudoku generateSudoku=new GenerateSudoku(level);
-                        String sudokuData=generateSudoku.getStringData();
-                        Intent intent=new Intent(StartActivity.this,MainActivity.class);
-                        intent.putExtra("data",sudokuData);
-                        closeProgressDialog();
+                        Message msg=new Message();
+                        msg.what=SHOW_LOADING_DIALOG;
+                        handler.sendMessage(msg);
+                        GenerateSudoku generateSudoku = new GenerateSudoku(level);
+                        String sudokuData = generateSudoku.getStringData();
+                        Intent intent = new Intent(StartActivity.this, MainActivity.class);
+                        intent.putExtra("data", sudokuData);
+                        Message msg1=new Message();
+                        msg1.what=CLOSE_LOADING_DIALOG;
+                        handler.sendMessage(msg1);
                         startActivity(intent);
 
                     }
@@ -44,11 +79,11 @@ public class StartActivity extends Activity{
 
             }
         });
-        levelButton=(Button)findViewById(R.id.level_button);
+        levelButton = (Button) findViewById(R.id.level_button);
         levelButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                LevelDialog levelDialog=new LevelDialog(StartActivity.this);
+                LevelDialog levelDialog = new LevelDialog(StartActivity.this);
                 levelDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
                     @Override
                     public void onDismiss(DialogInterface dialog) {
@@ -78,20 +113,47 @@ public class StartActivity extends Activity{
 
             }
         });
-    }
-    private void showProgressDialog(){
-        if(progressDialog==null){
-            progressDialog=new ProgressDialog(this);
-            progressDialog.setMessage("waiting...");
-            progressDialog.setCanceledOnTouchOutside(false);
-        }
-        progressDialog.show();
-    }
-    private void closeProgressDialog(){
-        if(progressDialog!=null){
-            progressDialog.dismiss();
-        }
+
     }
 
+    private void showProgressDialog() {
+        if (mLoadingDialog == null) {
+            mLoadingDialog = createLoadingDialog(this, "生成初盘中...");
+            mLoadingDialog.setCanceledOnTouchOutside(false);
+        }
+        mLoadingDialog.show();
+    }
+
+    private void closeProgressDialog() {
+        if (mLoadingDialog != null) {
+            mLoadingDialog.dismiss();
+        }
+        mLoadingDialog=null;
+
+    }
+
+    private Dialog createLoadingDialog(Context context, String msg) {
+        Log.d("StartActivity","创建LoadingDialog创建LoadingDialog创建LoadingDialog创建LoadingDialog");
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View v = inflater.inflate(R.layout.loadingdialog_layout, null);// 得到加载view
+        LinearLayout layout = (LinearLayout) v.findViewById(R.id.loadingdialog_view);// 加载布局
+        // main.xml中的ImageView
+        ImageView spaceshipImage = (ImageView) v.findViewById(R.id.img);
+        TextView tipTextView = (TextView) v.findViewById(R.id.tipTextView);// 提示文字
+        // 加载动画
+        Animation hyperspaceJumpAnimation = AnimationUtils.loadAnimation(
+                context, R.anim.loadingdialog_anim);
+        // 使用ImageView显示动画
+        spaceshipImage.startAnimation(hyperspaceJumpAnimation);
+        tipTextView.setText(msg);// 设置加载信息
+
+        Dialog loadingDialog = new Dialog(context, R.style.loading_dialog);// 创建自定义样式dialog
+
+        loadingDialog.setCancelable(true);// 可以用“返回键”取消
+        loadingDialog.setContentView(layout, new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT));// 设置布局
+        return loadingDialog;
+    }
 
 }
